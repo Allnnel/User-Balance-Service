a514ccc68a1282013e2765fbb7165361d4de700d
Rhogoro Norine <rhogoron@un-l5.kzn.21-school.ru>
2024-02-18 12:47:19 +0300

    testing

--- server/src/test/java/PostmanCollectionTest.java
+++ server/src/test/java/PostmanCollectionTest.java
@@ -1,6 +1,7 @@
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 
@@ -12,13 +13,12 @@ import java.io.InputStreamReader;
 import static org.junit.Assert.assertEquals;
 
 public class PostmanCollectionTest {
-
     @Test
     public void testPostmanCollection() throws IOException {
         String collectionUrl = "https://dev-booking.agilecyber.com/api/user/auth/mail-sign-in";
 
         HttpClient client = HttpClients.createDefault();
-        HttpGet request = new HttpGet(collectionUrl);
+        HttpPost request = new HttpPost(collectionUrl); // Используем метод POST
 
         HttpResponse response = client.execute(request);
 
@@ -34,4 +34,5 @@ public class PostmanCollectionTest {
         }
         System.out.println("Response Body: " + responseBody.toString());
     }
+
 }
