df5fcc838fbb69cb2768f786e27a328f51c6f0f1
Rhogoro Norine <rhogoron@un-l5.kzn.21-school.ru>
2024-02-19 15:56:52 +0300

    new tests

--- server/src/test/java/BalanceControllerTest.java
+++ server/src/test/java/BalanceControllerTest.java
@@ -1,11 +1,13 @@
 import com.example.UserBalanceServiceApplication;
 import com.example.controller.BalanceController;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,6 +27,28 @@ public class BalanceControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("balancesPage"));
     }
+//
+//    // 404
+    @Test
+    public void testPutUsersPage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/balances"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/balances"));
+    }
 
-    // Добавьте другие тесты для методов контроллера здесь
+    @Test
+    public void testPutUsersBodyPage() throws Exception {
+        // Создание JSON объекта для отправки
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("userLogin", "rhogoron");
+        requestBody.put("amount", "100");
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/balances")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody.toString()))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("redirect:/balances"));
+    }
+
+
 }
