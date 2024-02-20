ae8643491126c862fc846928aa36643c19277827
Alana Enikeeva <120022626+Allnnel@users.noreply.github.com>
2024-02-20 17:09:43 +0300

    Update BalanceServiceImpl.java

--- server/src/main/java/com/example/service/BalanceServiceImpl.java
+++ server/src/main/java/com/example/service/BalanceServiceImpl.java
@@ -8,7 +8,6 @@ import com.example.repository.BalanceRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
