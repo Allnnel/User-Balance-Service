5975ba19faea1a24bf8139befa3b9f43f2e0af4c
Rhogoro Norine <rhogoron@un-l5.kzn.21-school.ru>
2024-02-20 12:33:42 +0300

    new

--- server/src/test/java/ControllerTest.java
+++ server/src/test/java/ControllerTest.java
@@ -2,6 +2,7 @@ import com.example.UserBalanceServiceApplication;
 
 import com.example.model.Balance;
 import com.example.model.User;
+import com.example.repository.BalanceRepository;
 import com.example.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -18,9 +19,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.PrintStream;
 import java.time.LocalDate;
+
+import static java.lang.Thread.sleep;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = UserBalanceServiceApplication.class)
@@ -31,6 +35,8 @@ public class ControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private BalanceRepository balanceRepository;
 
 
     // ------------------- GET ------------------------------------
@@ -56,46 +62,38 @@ public class ControllerTest {
 
     @Test
     public void testPostUser() throws Exception {
-        // Создание объекта User
         User user = new User();
         user.setLogin("example_user");
         user.setPasswordHash("password666");
         user.setEmail("user@example.com");
         user.setBirthDay("1990-05-15");
         user.setMobilePhone("123-456-7890");
-
         // Преобразование объекта User в JSON
         ObjectMapper objectMapper = new ObjectMapper();
         String userRequestBody = objectMapper.writeValueAsString(user);
-
         // Отправка POST-запроса для сохранения пользователя
         mockMvc.perform(MockMvcRequestBuilders.post("/users")
                         .content(userRequestBody)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/users"));
-
         // Получаем сохраненного пользователя из базы данных
         User savedUser = userRepository.findByLogin("example_user").get(); // Предполагается, что у вас есть UserRepository
-
-        // Теперь создаем баланс с использованием сохраненного пользователя
+        // создаем баланс с использованием сохраненного пользователя
         Balance balance = new Balance(savedUser, 300);
         String balanceRequestBody = objectMapper.writeValueAsString(balance);
-
         // Отправка POST-запроса для сохранения баланса
         mockMvc.perform(MockMvcRequestBuilders.post("/balances")
                         .content(balanceRequestBody)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/balances"));
-
-
         User user5 = userRepository.findByLogin("example_user").get();
         user5.setEmail("5555555");
         // Преобразование объекта User в JSON
         ObjectMapper objectMapper4 = new ObjectMapper();
+        // Отправка PUT-запроса для сохранения баланса
         String requestBody = objectMapper4.writeValueAsString(user5);
-
         mockMvc.perform(MockMvcRequestBuilders.put("/users")
                         .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON))
@@ -103,6 +101,48 @@ public class ControllerTest {
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/users"));
     }
 
+    // ------------------- DELETE ------------------------------------
+    @Test
+    @Transactional
+    public void testDeleteUser() throws Exception {
+        User user = new User();
+        user.setLogin("deleteUser");
+        user.setPasswordHash("password666");
+        user.setEmail("user@example.com");
+        user.setBirthDay("1990-05-15");
+        user.setMobilePhone("123-456-7890");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userRequestBody = objectMapper.writeValueAsString(user);
+
+
+        // Отправка PUT-запроса для сохранения пользователя
+        mockMvc.perform(MockMvcRequestBuilders.put("/users")
+                        .content(userRequestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/users"));
+
+
+        User newUser = userRepository.findByLogin("deleteUser").get();
+        Balance balance = new Balance(newUser, 200);
+        String balanceRequestBody = objectMapper.writeValueAsString(balance);
+        mockMvc.perform(MockMvcRequestBuilders.put("/balances")
+                        .content(balanceRequestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/balances"));
+
+
+        String login = "deleteUser";
+        String userLogin = "deleteUser";
+        mockMvc.perform(MockMvcRequestBuilders.delete("/balances/{userLogin}", userLogin))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/balances"));
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/users/{login}", login))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/users"));
+    }
 
 
 
